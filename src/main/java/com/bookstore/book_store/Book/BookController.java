e52f41566ea8894c490adffb087470e99a60b95c
avidandrade <trejoavid@gmail.com>
2025-01-29 23:25:41 -0500

    Implemented google books api.

--- src/main/java/com/bookstore/book_store/Book/BookController.java
+++ src/main/java/com/bookstore/book_store/Book/BookController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -45,7 +46,12 @@ public class BookController {
     @PatchMapping("/books/{id}")
     public Book updateBook(@PathVariable Long id, @RequestBody Book book){
         return bookService.updateBook(id, book);
-    } 
+    }
+
+    @GetMapping("/searchbooks")
+    public String getBookFromAPI(@RequestParam String query){
+        return bookService.fetchBooks(query);  
+    }
 
 
 }
--- src/main/java/com/bookstore/book_store/Book/BookService.java
+++ src/main/java/com/bookstore/book_store/Book/BookService.java
@@ -6,6 +6,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import jakarta.persistence.EntityNotFoundException;
 
@@ -14,6 +15,25 @@ public class BookService {
 
     @Autowired
     private BookRepository bookRepository;
+    private final int maxResults = 5;
+
+    private final String API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
+
+    //Fetching books from API
+    public String fetchBooks(String query){
+        RestTemplate restTemplate = new RestTemplate();
+        String url = API_URL + query + "&maxResults=" + maxResults;
+        try{
+            //The query is created here that creates a url from our application, parameter of userinput and maxResults.
+
+            //RestTemplate object is creating a get request to the google books api by sending the query url and storing the payload in response.
+            String response = restTemplate.getForObject(url, String.class);
+            return response;
+        }catch(Exception e){
+            e.printStackTrace();
+            return "Error fetching books";
+        }
+    }
     
     public List<Book> getAllBooks(){ 
         return bookRepository.findAll();
