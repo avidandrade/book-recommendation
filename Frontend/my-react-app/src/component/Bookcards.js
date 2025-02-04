8625098c8e4961623c28d3bd5319ce084d92e653
avidandrade <trejoavid@gmail.com>
2025-02-03 22:31:51 -0500

    Added fetchBooks and handleSaveBook functions.

--- Frontend/my-react-app/src/component/Bookcards.js
+++ Frontend/my-react-app/src/component/Bookcards.js
@@ -2,24 +2,55 @@ import React, { useState, useEffect } from 'react';
 import './Bookcards.css';
 
 const BookCards = () => {
-    const [books, setBooks] = useState([]);
+    const[books, setBooks] = useState([]);
+    const[query, setQuery] = useState('');
 
-    useEffect(() => {
-        fetchBooks();
-    }, []);
 
-    const fetchBooks = async () => {
-        try {
-            const response = await fetch('http://localhost:8080/searchbooks?query=fun');
+    const fetchBooks = async(SearchQuery) => {
+        try{
+            const response = await fetch(`http://localhost:8080/searchbooks?query=${SearchQuery}`);
             const data = await response.json();
             setBooks(data);
-        } catch (error) {
-            console.error('Error fetching books:', error);
+        }catch(error){
+            console.error('Error fetching books from api', error);
+        }
+    }
+
+    const handleInputChange = (event) => {
+        setQuery(event.target.value);
+    }
+
+    const handleFormSubmit = (event) => {
+        event.preventDefault();
+        fetchBooks(query);
+    };
+
+    const handleSaveBook = async (book) =>{
+        try{
+            const response = await fetch(`http://localhost:8080/createBook`, {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(book),
+            })
+
+        }catch(error){
+            console.error('Error saving book', error);
         }
     };
 
     return (
         <div className="book-cards-container">
+            <form onSubmit={handleFormSubmit}>
+                <input
+                    type="text"
+                    value={query}
+                    onChange={handleInputChange}
+                    placeholder="Search for books"
+                />
+                <button type="submit">Search</button>
+            </form>
             {books.map((book, index) => (
                 <div key={index} className="book-card">
                     <h3>{book.title}</h3>
@@ -27,6 +58,7 @@ const BookCards = () => {
                     <p><strong>Description:</strong> {book.description}</p>
                     <p><strong>ISBN:</strong> {book.isbn}</p>
                     <p><strong>Genre:</strong> {book.genre}</p>
+                    <button onClick={() => handleSaveBook(book)}>Save</button>
                 </div>
             ))}
         </div>
