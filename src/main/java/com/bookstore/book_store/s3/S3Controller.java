92f553eee5f81d7072666621f849f720e50913f2
avidandrade <trejoavid@gmail.com>
2025-02-04 20:12:28 -0500

    Changed S3 service classs to save the cover images from urls.

--- src/main/java/com/bookstore/book_store/s3/S3Controller.java
+++ src/main/java/com/bookstore/book_store/s3/S3Controller.java
@@ -1,12 +1,9 @@
 package com.bookstore.book_store.s3;
 
-import java.io.IOException;
-
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.multipart.MultipartFile;
+
 
 
 @RestController
@@ -17,18 +14,17 @@ public class S3Controller {
         this.s3Service = s3Service;
     }
 
-    @PostMapping("/upload")
-    public ResponseEntity<String> uploadFile(@RequestParam("file") MultipartFile multipartFile) {
-        String key = multipartFile.getOriginalFilename(); //get name of file
-        try{
-            byte[] fileBytes = multipartFile.getBytes();
-            s3Service.putObject(s3Service.getBucketName(), key, fileBytes);
-            return ResponseEntity.ok("File uploaded successfully " + key);
 
-        }catch(IOException e){
+    @PostMapping("/uploadCover")
+    public String uploadBookCover(@RequestParam("imageUrl") String url, @RequestParam("title") String title) {  
+        try{
+            return s3Service.uploadImageToS3(url,title);
+        }catch(Exception e){
             e.printStackTrace();
-            return ResponseEntity.status(500).body( "Error uploading file");
+            return "Error uploading file";
         }
+        
     }
+    
 
 }
--- src/main/java/com/bookstore/book_store/s3/S3Service.java
+++ src/main/java/com/bookstore/book_store/s3/S3Service.java
@@ -1,5 +1,11 @@
 package com.bookstore.book_store.s3;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,16 +25,38 @@ public class S3Service {
 
     @Value("${aws.bucketName}")
     private String bucketName;
-    
-    public void putObject(String bucketName, String key, byte[] file){
-        PutObjectRequest objectRequest = PutObjectRequest.builder()
-                .bucket(bucketName)
-                .key(key)
-                .build();
-        s3Client.putObject(objectRequest,RequestBody.fromBytes(file));
-    }
 
-    public String getBucketName() {
-        return bucketName;
+    @Value("${aws.accesskey}")
+    private String awsKey;
+
+    
+    public String uploadImageToS3(String imageUrl, String title) {
+        try {
+            // Download the image
+            URL url = new URL(imageUrl);
+            File tempFile;
+            try (InputStream inputStream = url.openStream()) {
+                tempFile = File.createTempFile(title, ".jpg");
+                FileOutputStream outputStream = new FileOutputStream(tempFile);
+                byte[] buffer = new byte[2048];
+                int bytesRead;
+                while ((bytesRead = inputStream.read(buffer)) != -1) {
+                    outputStream.write(buffer, 0, bytesRead);
+                }   outputStream.close();
+            }
+
+            // Upload the image to S3
+            String key = "bookcovers/" + tempFile.getName();
+            PutObjectRequest putObjectRequest = PutObjectRequest.builder()
+                    .bucket(bucketName)
+                    .key(awsKey)
+                    .build();
+            s3Client.putObject(putObjectRequest, RequestBody.fromFile(tempFile));
+
+            // Return the S3 URL
+            return s3Client.utilities().getUrl(builder -> builder.bucket(bucketName).key(key)).toExternalForm();
+        } catch (IOException e) {
+            return "Error uploading file";
+        }
     }
 }
