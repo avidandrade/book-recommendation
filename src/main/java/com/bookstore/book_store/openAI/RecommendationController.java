a6edbbcbe4f1af9a577a484be107721018de001d
avidandrade <trejoavid@gmail.com>
2025-02-13 00:27:17 -0500

    commenting out the old ai model files. Preparing for new one.

--- src/main/java/com/bookstore/book_store/openAI/RecommendationController.java
+++ src/main/java/com/bookstore/book_store/openAI/RecommendationController.java
@@ -1,16 +1,16 @@
 package com.bookstore.book_store.openAI;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
 
-@RestController
-public class RecommendationController {
+// @RestController
+// public class RecommendationController {
 
-    @Autowired
-    private openAIService openAIService;
+//     @Autowired
+//     private openAIService openAIService;
 
-    @GetMapping("/recommendations")
-    public String getRecommendations(@RequestParam String preferences) {
-        return openAIService.getRecommendations(preferences);
-    }
-}
+//     @GetMapping("/recommendations")
+//     public String getRecommendations(@RequestParam String preferences) {
+//         return openAIService.getRecommendations(preferences);
+//     }
+// }
