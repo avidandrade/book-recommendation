e28eacbf232b58c240af8dbb9daff6564a3dba6f
avidandrade <trejoavid@gmail.com>
2025-02-18 19:58:25 -0500

    removed unused import.

--- src/main/java/com/bookstore/book_store/Ollama3/RecommendationController.java
+++ src/main/java/com/bookstore/book_store/Ollama3/RecommendationController.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.bookstore.book_store.Book.Book;
-
 @RestController
 public class RecommendationController {
 
