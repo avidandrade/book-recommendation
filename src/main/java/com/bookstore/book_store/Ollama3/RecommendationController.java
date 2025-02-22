af8975acd6fdf2ab64d4f4f90166c8739dae91a4
avidandrade <trejoavid@gmail.com>
2025-02-21 21:29:30 -0500

    Added controller method to retrieve summary and review of book.

--- src/main/java/com/bookstore/book_store/Ollama3/RecommendationController.java
+++ src/main/java/com/bookstore/book_store/Ollama3/RecommendationController.java
@@ -3,7 +3,9 @@ package com.bookstore.book_store.Ollama3;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RecommendationController {
@@ -14,5 +16,15 @@ public class RecommendationController {
     @GetMapping("/generate")
     public List<String> generateQuery(@RequestParam String emotion){
         return ollamaService.getRecommendedBookTitle(emotion);
+    }
+
+    @GetMapping("/summary")
+    public String getSummary(@RequestParam String title){
+        return ollamaService.getBookSummary(title);
+    }
+
+    @GetMapping("/review")
+    public String getReview(@RequestParam String title, @RequestParam int rating){
+        return ollamaService.getBookReview(title, rating);
     }
 }
