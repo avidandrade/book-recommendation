0e937063daf1d1c5bb7bbfe7a02d4a71ed82231f
avidandrade <trejoavid@gmail.com>
2025-02-25 23:40:15 -0500

    Model for user.

---
+++ src/main/java/com/bookstore/book_store/User/User.java
@@ -0,0 +1,57 @@
+package com.bookstore.book_store.User;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String auth0Id;
+
+    @Column(unique = true)
+    private String email;
+
+    private String name;
+
+    public User(String auth0Id, String email, String name) {
+        this.auth0Id = auth0Id;
+        this.email = email;
+        this.name = name;
+    }
+
+    public String getAuth0Id() {
+        return auth0Id;
+    }
+
+    public void setAuth0Id(String auth0Id) {
+        this.auth0Id = auth0Id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+}
