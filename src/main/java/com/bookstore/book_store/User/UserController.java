6527b0006efa92cd9fd326b267ccbcff1ff212d5
avidandrade <trejoavid@gmail.com>
2025-02-26 23:51:04 -0500

    Temp commented out user files.

--- src/main/java/com/bookstore/book_store/User/UserController.java
+++ src/main/java/com/bookstore/book_store/User/UserController.java
@@ -1,19 +1,19 @@
-package com.bookstore.book_store.User;
+// package com.bookstore.book_store.User;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+// import org.springframework.web.bind.annotation.GetMapping;
+// import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class UserController {
+// @RestController
+// public class UserController {
 
-    @Autowired
-    private UserService userService;
+//     @Autowired
+//     private UserService userService;
 
-    @GetMapping("/user")
-    public OidcUser getUser(OidcUser oidcUser) {
-        userService.saveUser(oidcUser);
-        return oidcUser;
-    }
-}
+//     @GetMapping("/user")
+//     public OidcUser getUser(OidcUser oidcUser) {
+//         userService.saveUser(oidcUser);
+//         return oidcUser;
+//     }
+// }
--- src/main/java/com/bookstore/book_store/User/UserService.java
+++ src/main/java/com/bookstore/book_store/User/UserService.java
@@ -1,23 +1,23 @@
-package com.bookstore.book_store.User;
+// package com.bookstore.book_store.User;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.oauth2.core.oidc.user.OidcUser;
-import org.springframework.stereotype.Service;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.security.oauth2.core.oidc.user.OidcUser;
+// import org.springframework.stereotype.Service;
 
-@Service
-public class UserService {
+// @Service
+// public class UserService {
 
-    @Autowired
-    private UserRepository userRepository;
+//     @Autowired
+//     private UserRepository userRepository;
 
-    public void saveUser(OidcUser oidcUser) {
-        String auth0Id = oidcUser.getSubject();
-        String email = oidcUser.getEmail();
-        String name = oidcUser.getFullName();
+//     public void saveUser(OidcUser oidcUser) {
+//         String auth0Id = oidcUser.getSubject();
+//         String email = oidcUser.getEmail();
+//         String name = oidcUser.getFullName();
 
-        User user = userRepository.findByAuth0Id(auth0Id)
-                .orElse(new User(auth0Id, email, name));
+//         User user = userRepository.findByAuth0Id(auth0Id)
+//                 .orElse(new User(auth0Id, email, name));
 
-        userRepository.save(user);
-    }
-}
+//         userRepository.save(user);
+//     }
+// }
