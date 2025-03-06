4dc6c6ce6bb7d1748844ec76313d1089f9e98cdd
avidandrade <trejoavid@gmail.com>
2025-03-06 16:38:50 -0500

    Adjusted some styling for header.

--- book-recommendation/src/App.jsx
+++ book-recommendation/src/App.jsx
@@ -6,8 +6,9 @@ function App() {
 
   return (
 
-    <div>
+    <div className='p- max-w-4xl mx-auto'>
         <h1>Book Recommendation</h1>
+        <p className='p-3'>Discover book recommendations based on your feelings!</p>
         <BookCards />
     </div>
   )
