d42eee564a90a47bf7e1265168df08e20d16e409
avidandrade <trejoavid@gmail.com>
2025-03-18 21:15:38 -0400

    fetch review and summary of book page.

--- book-recommendation/src/app/BookInfo.jsx
+++ book-recommendation/src/app/BookInfo.jsx
@@ -5,7 +5,8 @@ const BookInfo = () => {
   const { id } = useParams();
   const [book, setBook] = useState(null);
   const [loading, setLoading] = useState(true);
-  const [reviews, setReviews] = useState([]);
+  const [reviewSummary, setReviewSummary] = useState([]);
+  const [summary, setSummary] = useState([]);
 
   useEffect(() => {
     const fetchBookInfo = async () => {
@@ -15,14 +16,16 @@ const BookInfo = () => {
         setBook(data);
 
         if(data.title && data.rating){
-          const reviewResponse = await fetch(`http://localhost:8080/review?title=${data.title}&rating=${data.rating}`);
+          const [reviewResponse, summary] = await Promise.all([
+            fetch(`http://localhost:8080/review?title=${data.title}&rating=${data.rating}`),
+            fetch(`http://localhost:8080/summary?title=${data.title}`)
+          ]);
+
           const reviewText = await reviewResponse.text();
-          const reviewData = reviewText.split('.').map((sentence, index) => ({
-            id: index,
-            comment: sentence.trim(),
-            rating: data.rating 
-          }));
-          setReviews(reviewData);
+          const summaryText = await summary.text();
+
+          setReviewSummary(reviewText);
+          setSummary(summaryText);
         }
       } catch (error) {
         console.error('Error fetching book information', error);
@@ -56,16 +59,17 @@ const BookInfo = () => {
         <strong>ISBN:</strong> {book.isbn || 'N/A'}
       </p>
       <div className="mt-4">
-        <h2 className="text-xl font-bold">Reviews</h2>
-        {reviews.length > 0 ? (
-          reviews.map((review, index) => (
-            <div key={index} className="mt-2">
-              <p className="text-sm"><strong>Rating:</strong> {review.rating}</p>
-              <p className="text-sm">{review.comment}</p>
-            </div>
-          ))
+        <h2 className="text-xl font-bold">Review</h2>
+        {reviewSummary ? (
+          <p className="text-sm">{reviewSummary}</p>
         ) : (
           <p>No reviews available.</p>
+        )}
+        <h2 className="text-xl font-bold p-2">Summary</h2>
+        {summary ? (
+          <p className="text-sm">{summary}</p>
+        ) : (
+          <p>No summary available.</p>
         )}
       </div>
     </div>
