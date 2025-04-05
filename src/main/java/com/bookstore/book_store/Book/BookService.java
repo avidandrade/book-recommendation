48741f533e2e69326b8924593c86032314eba09e
avidandrade <trejoavid@gmail.com>
2025-04-05 19:54:37 -0400

    Improving bookservice class with more seperation of classes

--- src/main/java/com/bookstore/book_store/Book/BookService.java
+++ src/main/java/com/bookstore/book_store/Book/BookService.java
@@ -18,101 +18,34 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 @Service
 public class BookService {
 
-    @Autowired
     private BookRepository bookRepository;
-
-    @Autowired
     private OllamaService ollamaService;
-
-    @Autowired
     private S3Service s3Service;
+    private GoogleService googleService;
 
-    private final int maxResults = 5;
-
-    private final String API_URL = "https://www.googleapis.com/books/v1/volumes?q=";
-    private final RestTemplate restTemplate = new RestTemplate();
-    private final ObjectMapper mapper = new ObjectMapper();
+    public BookService(BookRepository bookRepository, OllamaService ollamaService,S3Service s3Service,GoogleService googleService){
+        this.bookRepository = bookRepository;
+        this.ollamaService = ollamaService;
+        this.s3Service = s3Service;
+        this.googleService = googleService;
+    }
 
     public List<Book> fetchRecommendedBooks(String userInput) {
         //Get a book title from Ollama AI
         List<String> bookTitles = ollamaService.getRecommendedBookTitle(userInput);
-        System.out.println(bookTitles);
-        if (bookTitles == null || bookTitles.isEmpty()) {
-            throw new RuntimeException("Ollama failed to generate a book title.");
-        }
-
-        // Fetch book details from Google Books API**
-        return fetchBooks(bookTitles);
+        return googleService.fetchRecommendedBooks(bookTitles);
     }
 
     public List<Book> fetchMoreBooks(String userInput, List<String> titles) {
         //Titles are sent so that the model avoids sending the same book.
         List<String> bookTitles = ollamaService.getMoreBooks(userInput, titles);
         if (bookTitles == null || bookTitles.isEmpty()) {
-            throw new RuntimeException("Ollama failed to generate a book title.");
+            throw new RuntimeException("Ollama failed to generate more recommended book titles!");
         }
 
         // Fetch book details from Google Books API**
-        return fetchBooks(bookTitles);
-    }
-
-    //Fetching books from API
-    public List<Book> fetchBooks(List<String> bookTitles) {
-            List<Book> books = new ArrayList<>();
-
-            for(String title : bookTitles){
-                String url = API_URL + title + "&maxResults=" + maxResults;
-
-                String response = restTemplate.getForObject(url, String.class);
-                books.add(parseResponse(response));
-            }
-            return books;
-    }
-    
-    public Book parseResponse(String response){
-        try{
-            JsonNode root = mapper.readTree(response);
-            JsonNode items = root.path("items");
-            if (items.isArray()) {
-                for (JsonNode item : items) {
-                    String title = item.path("volumeInfo").path("title").asText();
-                    String authors = item.path("volumeInfo").path("authors").toString();
-                    String description = item.path("volumeInfo").path("description").asText();
-                    int rating = item.path("volumeInfo").path("averageRating").asInt();
-                    
-                    // Extract ISBN
-                    String isbn = "";
-                    JsonNode industryIdentifiers = item.path("volumeInfo").path("industryIdentifiers");
-                    if (industryIdentifiers.isArray()) {
-                        for (JsonNode identifier : industryIdentifiers) {
-                            if ("ISBN_13".equals(identifier.path("type").asText())) {
-                                isbn = identifier.path("identifier").asText();
-                                break;
-                            }
-                        }
-                    }
-
-                    // Extract genre (categories)
-                    String genre = "";
-                    JsonNode categories = item.path("volumeInfo").path("categories");
-                    if (categories.isArray() && categories.size() > 0) {
-                        genre = categories.get(0).asText();
-                    }
-                    String coverImageUrl = "";
-                    JsonNode imageLinks = item.path("volumeInfo").path("imageLinks");
-                    if (imageLinks.has("thumbnail")) {
-                        coverImageUrl = imageLinks.path("thumbnail").asText();
-                    }
-
-                    return new Book(title, authors, genre,description, isbn, coverImageUrl, rating);
-                }       
-            }
-        }catch(IOException e){
-            return null;
-        }
-        return null;
-    }
-                                                                                                   
+        return googleService.fetchRecommendedBooks(bookTitles);
+    }                                                                                         
     public List<Book> getAllBooks(){ 
         return bookRepository.findAll();
     }
