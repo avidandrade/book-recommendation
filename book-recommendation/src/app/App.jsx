9c8536f3c251d3473285a140dd96973f336d9a52
avidandrade <trejoavid@gmail.com>
2025-04-13 17:33:07 -0400

    Added new routes.

--- book-recommendation/src/app/App.jsx
+++ book-recommendation/src/app/App.jsx
@@ -1,16 +1,30 @@
 
 import BookCards from './BookCards.jsx'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import BookInfo from './BookInfo.jsx';
+import SignUp from './routes/sign-up.jsx';
+import Login from './routes/Login.jsx';
+import { useState, useEffect } from 'react';
+import ForgotPassword from './routes/forgot-password.jsx';
 import './App.css'
 
 function App() {
+  const [isAuthenticated, setIsAuthenticated] = useState(!!localStorage.getItem('token'));
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    setIsAuthenticated(!!token);
+  }, []);
 
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<BookCards />} />
-        <Route path="/books/:id" element={<BookInfo/>} />
+        <Route path="/" element={<Navigate to="/login" />} />
+        <Route path="/login" element={<Login onLogin={() => setIsAuthenticated(true)} />} />
+        <Route path="/sign-up" element={<SignUp/>}/>
+        <Route path="/forgot-password" element={<ForgotPassword/>}/>
+        <Route path="/books" element={isAuthenticated ? <BookCards /> : <Navigate to="/login"/>}/>
+        <Route path="/books/:id" element={isAuthenticated ? <BookInfo/> : <Navigate to="/login"/>} />
       </Routes>
     </Router>
   )
