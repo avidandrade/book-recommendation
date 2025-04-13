42bd3b05b5f962b1e56789c178aa3b001030c53c
avidandrade <trejoavid@gmail.com>
2025-04-12 22:16:47 -0400

    Setting up for supabase auth.

--- book-recommendation/src/app/Login.jsx
+++ book-recommendation/src/app/Login.jsx
@@ -1,23 +1,6 @@
 import React, { useEffect } from 'react';
-import { useAuth0 } from '@auth0/auth0-react';
-import { useNavigate } from 'react-router-dom';
+import { supabase } from '../supabaseClient';
 
-const Login = () => {
-  const { loginWithRedirect, isAuthenticated } = useAuth0();
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    if (isAuthenticated) {
-      navigate('/book-cards');
-    }
-  }, [isAuthenticated, navigate]);
-
-  return (
-    <div>
-      <h2>Please log in to view book recommendations.</h2>
-      <button onClick={() => loginWithRedirect()}>Log In</button>
-    </div>
-  );
+const handleLogin = async () => { 
+  
 };
-
-export default Login;
