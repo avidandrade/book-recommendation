b5c329394002bdd0d602d9bbaea4f77a0e7126b5
avidandrade <trejoavid@gmail.com>
2025-04-14 20:02:47 -0400

    Removing unused classes. Supabase is handling users.

--- src/main/java/com/bookstore/book_store/User/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.bookstore.book_store.User;
-
-import java.util.Optional;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    Optional<User> findByAuth0Id(String auth0Id);
-}
