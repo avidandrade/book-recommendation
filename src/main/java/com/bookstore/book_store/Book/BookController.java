a89c61be79c0e0e6567550d1eddcb341c4ae62e6
avidandrade <trejoavid@gmail.com>
2025-04-26 18:58:00 -0400

    Uploaded model attribute logic for new token from cookie.

--- src/main/java/com/bookstore/book_store/Book/BookController.java
+++ src/main/java/com/bookstore/book_store/Book/BookController.java
@@ -5,13 +5,13 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,7 +30,7 @@ public class BookController {
     }
 
     @ModelAttribute
-    public void extractUserId(@RequestHeader("Authorization") String token){
+    public void extractUserId(@CookieValue(value = "authToken", required = false) String token){
         this.userId = jwtUtil.extractUserId(token);
     }
     
