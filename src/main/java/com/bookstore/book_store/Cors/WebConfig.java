41325b463e5f371f62d9ba4464cd642fb93f4a0a
avidandrade <trejoavid@gmail.com>
2025-04-26 18:58:14 -0400

    Adjusted cors for new cookie changes.

--- src/main/java/com/bookstore/book_store/Cors/WebConfig.java
+++ src/main/java/com/bookstore/book_store/Cors/WebConfig.java
@@ -12,7 +12,11 @@ public class WebConfig {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(org.springframework.web.servlet.config.annotation.CorsRegistry registry) {
-                registry.addMapping("/**").allowedOrigins("http://localhost:5173").allowedMethods("GET", "POST", "PUT", "DELETE");
+                registry.addMapping("/**")
+                .allowedOrigins("http://localhost:5173")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("*")
+                .allowCredentials(true);
             }
         };
     }
