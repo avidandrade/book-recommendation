942dd7b437082b6cca76e8713daf8f8fddced63f
avidandrade <trejoavid@gmail.com>
2025-04-29 16:33:43 -0400

    Test for new authentication security.

---
+++ src/main/java/com/bookstore/book_store/Auth0/JwtAuthenticationFilter.java
@@ -0,0 +1,62 @@
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import com.bookstore.book_store.Auth0.JwtUtil;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import io.jsonwebtoken.Claims;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+@Component
+public class JwtAuthenticationFilter extends OncePerRequestFilter{
+
+    private final JwtUtil jwtUtil;
+
+    public JwtAuthenticationFilter(JwtUtil jwtUtil) {
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String authToken = null;
+
+        if(request.getCookies() != null){
+            for(Cookie cookie : request.getCookies()){
+                if("authToken".equals(cookie.getName())){
+                    authToken = cookie.getValue();
+                    break;
+                }
+            }
+        }
+
+        if(authToken != null){
+            try{
+                Claims claims = jwtUtil.validateToken(authToken);
+                String userId = claims.getSubject();
+
+                UsernamePasswordAuthenticationToken authentication =
+                            new UsernamePasswordAuthenticationToken(userId, null, new ArrayList<>());
+                
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+
+
+            }catch(Exception e){
+                response.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
+
+            }
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
