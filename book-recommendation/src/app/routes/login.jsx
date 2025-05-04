e2bc4c0fd9dee966749054a0911973cd03b875a7
avidandrade <trejoavid@gmail.com>
2025-05-04 12:33:46 -0400

    Added title and small description to front page.

--- book-recommendation/src/app/routes/login.jsx
+++ book-recommendation/src/app/routes/login.jsx
@@ -62,6 +62,8 @@ export default function Login({onLogin}) {
     <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
       <div className="w-full max-w-sm">
         <div className="flex flex-col gap-6">
+        <h1>BookWise!</h1>
+        <p className='p-3'>Discover book recommendations based on your feelings!</p>
           <Card>
             <CardHeader>
               <CardTitle className="text-2xl">Login</CardTitle>
